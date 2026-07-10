88b240f5dc6c6804f3ff29f71af23841ee55cc0a
mrmatonski <mrmatonski@gmail.com>
2026-07-10 07:24:27 -0700

    Route new items to dynamic details

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -181,7 +181,10 @@ const NewItems = () => {
                         </div>
                       </div>
 
-                      <Link to="/item-details">
+                      <Link
+                        to={`/item-details/${item.nftId}`}
+                        state={{ item }}
+                      >
                         <img
                           src={item.nftImage}
                           className="lazy nft__item_preview"
@@ -190,7 +193,10 @@ const NewItems = () => {
                       </Link>
                     </div>
                     <div className="nft__item_info">
-                      <Link to="/item-details">
+                      <Link
+                        to={`/item-details/${item.nftId}`}
+                        state={{ item }}
+                      >
                         <h4>{item.title}</h4>
                       </Link>
                       <div className="nft__item_price">{item.price} ETH</div>
